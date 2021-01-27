7d78521a196240185acb36cbcc40d0da1ef2f968
CordarJovana <cordarjovana@gmail.com>
2021-01-28 00:48:00 +0100

    Ubacen sat na pocetnu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './index.css'
 
-const Home=()=>{
-    return(
-        <div className="sve">
-        <div classNamestyle="naslovislika" style={
-            {display: 'flex', justifyContent:'center', alignItems: 'center', height: '90vh'}
+
+
+class Home extends Component{
+    state={time:new Date()}
+
+    currentTime(){
+        this.setState({
+            time:new Date()
+        })
+    }
+
+    componentWillMount(){
+setInterval(()=>this.currentTime(),1000)
+    }
+
+
+    render(){
+        return(
+<div className="sve">
+<div classNamestyle="naslovislika" style={
+            { justifyContent:'center', alignItems: 'center', alignContent:'center', height: '20vh', display: 'flex'}
         }>
-            <h1>Bolnica H&H</h1>
+            
+            <h4>Vreme: {this.state.time.toLocaleTimeString()}</h4>
+            
+            
         </div>
+
+        <div classNamestyle="naslovislika" style={
+            { justifyContent:'center', alignItems: 'center', alignContent:'center', height: '40vh', display: 'flex'}
+        }>
+            <h1>Bolnica H&H </h1>
+         
+            
+            
         </div>
-    )
+
+        
+        </div>
+        )
+    }
 }
 
 export default Home
